7895291f81e16a64d63f042ad74e41de72235f59
linhnguyen <linh.nguyenthikhanh23@student.passerellesnumeriques.org>
2022-06-21 10:53:05 +0700

    add controller

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Input, File;
+use App\Http\Requests\addRoom;
+
+// use Illuminate\Support\Facades\Session;
+
+class PagesController extends Controller{
+    public function index(){
+        return view('index');
+    }
+    // public function about(){
+    //     return view('about');
+    // }
+    public function addRooms (addRoom $Request){
+        $image = $Request->file('roomImage');
+        $path = $image->move('images', $image->getClientOriginalName());
+
+
+        $newRoom = [
+            'name' => $Request->roomName,
+            'description' => $Request->roomDescription,
+            'price' => $Request->roomPrice,
+            'image' => $image->getClientOriginalName(),
+        ];
+
+        if (isset($_SESSION['rooms'])) {
+            $_SESSION['rooms'][] = $newRoom;
+        } else {
+            if (session_id() === '')
+                session_start();
+            $_SESSION['rooms'][] = $newRoom;
+        }
+        echo '<script>alert("Thêm phòng thành công")</script>';
+        return view('index');
+    }
+    public function destroy($id){
+        // if(isset($_SESSION['rooms']))
+        // $arr = $_SESSION['rooms'];
+        
+        
+        // return view('index')->with('arr',$arr);
+        dd($id);
+    }
+
+}
+
